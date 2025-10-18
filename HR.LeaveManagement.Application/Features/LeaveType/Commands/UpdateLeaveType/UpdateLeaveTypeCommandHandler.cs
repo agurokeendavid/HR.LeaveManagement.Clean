00002f9136ade5a847f8820b917e743b5c72cc7b
agurokeendavid <agurokeendavid@gmail.com>
2025-10-18 12:00:55 +0800

    Refactor LeaveType features and repository methods
    Refactored `IGenericRepository` to update `GetAsync` to return a list of entities. Added new commands, queries, and handlers for `LeaveType` operations, including create, update, delete, and retrieve details. Introduced `LeaveTypeDetailsDto` and updated AutoMapper profiles. Cleaned up project structure by removing unused folder references.

---
+++ HR.LeaveManagement.Application/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeaveType
+{
+    public class UpdateLeaveTypeCommandHandler : IRequestHandler<UpdateLeaveTypeCommand, Unit>
+    {
+        private readonly IMapper _mapper;
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public UpdateLeaveTypeCommandHandler(IMapper mapper, ILeaveTypeRepository leaveTypeRepository)
+        {
+            _mapper = mapper;
+            _leaveTypeRepository = leaveTypeRepository;
+        }
+
+        public async Task<Unit> Handle(UpdateLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            // validate incoming data
+
+            // convert to domain entity object
+            var leaveTypeToCreate = _mapper.Map<Domain.LeaveType>(request);
+
+            // update to database
+            await _leaveTypeRepository.UpdateAsync(leaveTypeToCreate);
+
+            // return unit value
+            return Unit.Value;
+        }
+    }
+}
