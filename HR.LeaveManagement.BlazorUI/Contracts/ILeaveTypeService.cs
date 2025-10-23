3abe9f8869301682d592b9e248368e6825e4e8c9
agurokeendavid <agurokeendavid@gmail.com>
2025-10-24 02:58:41 +0800

    Created project for UI

---
+++ HR.LeaveManagement.BlazorUI/Contracts/ILeaveTypeService.cs
@@ -0,0 +1,14 @@
+﻿using HR.LeaveManagement.BlazorUI.Models.LeaveTypes;
+using HR.LeaveManagement.BlazorUI.Services.Base;
+
+namespace HR.LeaveManagement.BlazorUI.Contracts
+{
+    public interface ILeaveTypeService
+    {
+        Task<List<LeaveTypeVM>> GetLeaveTypesAsync();
+        Task<LeaveTypeVM> GetLeaveTypeDetailsAsync(int id);
+        Task<Response<Guid>> CreateLeaveTypeAsync(LeaveTypeVM leaveType);
+        Task<Response<Guid>> UpdateLeaveTypeAsync(int id, LeaveTypeVM leaveType);
+        Task<Response<Guid>> DeleteLeaveTypeAsync(int id);
+    }
+}
