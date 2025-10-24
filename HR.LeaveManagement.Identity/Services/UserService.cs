828d7ed749129cbad00d895f044c5c6ed5c41804
agurokeendavid <agurokeendavid@gmail.com>
2025-10-25 01:43:18 +0800

    Add UserService and update identity project dependencies
    Introduced a new `UserService` to handle user-related operations, including retrieving employee details. Updated `HR.LeaveManagement.Identity.csproj` to include the `DbContext` folder and added the `Microsoft.Extensions.Identity.Core` package reference. Enhanced identity management capabilities by leveraging `UserManager<ApplicationUser>` from ASP.NET Core Identity.

---
+++ HR.LeaveManagement.Identity/Services/UserService.cs
@@ -0,0 +1,40 @@
+﻿using HR.LeaveManagement.Application.Contracts.Identity;
+using HR.LeaveManagement.Application.Models.Identity;
+using HR.LeaveManagement.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace HR.LeaveManagement.Identity.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserService(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<List<Employee>> GetEmployeesAsync()
+        {
+            var employees = await _userManager.GetUsersInRoleAsync("Employee");
+            return employees.Select(q => new Employee()
+            {
+                Id = q.Id,
+                Email = q.Email,
+                Firstname = q.FirstName,
+                Lastname = q.LastName
+            }).ToList();
+        }
+
+        public async Task<Employee> GetEmployeeAsync(string userId)
+        {
+            var employee = await _userManager.FindByIdAsync(userId);
+            return new Employee()
+            {
+                Email = employee.Email,
+                Id = employee.Id,
+                Firstname = employee.FirstName,
+                Lastname = employee.LastName
+            };
+        }
+    }
+}
